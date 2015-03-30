f6ed273838292f99dea8c81ad85141d9ccb2b04d
buynowdays <buynowdays@hotmail.com>
2015-03-30 13:25:32 +0800

    高亮处理

--- 3DSmartCity/3DSmartCity/HighLightVisitor.cpp
+++ 3DSmartCity/3DSmartCity/HighLightVisitor.cpp
@@ -10,3 +10,18 @@ HighLightVisitor::HighLightVisitor(void)
 HighLightVisitor::~HighLightVisitor(void)
 {
 }
+
+void HighLightVisitor::apply(osg::Node& node)
+{
+
+}
+
+void HighLightVisitor::apply(osg::Geode& node)
+{
+
+}
+
+void HighLightVisitor::apply(osg::Group& node)
+{
+
+}
--- 3DSmartCity/3DSmartCity/HighLightVisitor.h
+++ 3DSmartCity/3DSmartCity/HighLightVisitor.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <osg/NodeVisitor>
+#include <osg/Node>
+#include <osg/Group>
+#include <osg/Geode>
 class HighLightVisitor :
 	public osg::NodeVisitor
 {
@@ -7,8 +10,8 @@ public:
 	HighLightVisitor(void);
 	~HighLightVisitor(void);
 
-	void apply(Node& node);　　　//虚函数，访问各种节点类型，并执行访问器中的自定义操作
-　　void apply(Group& node);
-　　void apply(Geode& node);
+	virtual void apply(osg::Node& node);//虚函数，访问各种节点类型，并执行访问器中的自定义操作
+	virtual void apply(osg::Group& node);
+	virtual void apply(osg::Geode& node);
 };
 
